31ca6e46820ec244ea51f0718af45d6a940c6f47
Ryan To <ryankto1@gmail.com>
2020-02-22 20:19:22 -0600

    Player Movements

--- moveTick.py
+++ moveTick.py
@@ -6,40 +6,40 @@ import pygame
 #walls is the placeholder for the map detection
 #####
 
-def moveTick(pos1,pos2,velo1,velo2,height,walls,event,dim,pWidth,pHeight):#velo 1 is left right, velo 2 is up down. dim is bool, 1 if there is y and if there is gravity
+def moveTick(pos1,pos2,velo1,velo2,height,wall,event,dim,pWidth,pHeight):#velo 1 is left right, velo 2 is up down. dim is bool, 1 if there is y and if there is gravity
     if event.type == event.KEYDOWN:
         if event.key == event.K_A or event.key == event.K_LEFT:
             velo1=-20
-            if pos1+velo < wall  and not wall == 'portal':
+            if pos1+velo1 < wall  and not wall == 'portal':
                 for x in range(20):
                     if pos1+19-x == wall:#going left check
-                        velo = 19-x
-            pos1+=velo
+                        velo1 = 19-x
+            pos1+=velo1
         if event.key == event.K_D or event.key == event.K_RIGHT:
             velo1=20
-            if pos1+velo > wall  and not wall == 'portal':
+            if pos1+velo1 > wall  and not wall == 'portal':
                 for x in range(20):
                     if pos1+19-x == wall:#going left check
-                        velo = 19-x
+                        velo1 = 19-x
                         break
             pos1+=velo1
         if not dim:
             if event.key == event.K_S or event.key == event.K_DOWN:
                 velo2=20
-                if pos1+velo > wall  and not wall == 'portal':
+                if pos2+velo2 > wall  and not wall == 'portal':
                     for x in range(20):
-                        if pos1+19-x == wall:#going left check
-                            velo = 19-x
+                        if pos2+19-x == wall:#going left check
+                            velo2 = 19-x
                 pos2+=velo2
             if event.key == event.K_W or event.key == event.K_UP:
                 velo2=-20
-                if pos1+velo < wall  and not wall == 'portal':
+                if pos2+velo2 < wall  and not wall == 'portal':
                     for x in range(20):
-                        if pos1+19-x == wall:#going left check
-                            velo = 19-x
+                        if pos2+19-x == wall:#going left check
+                            velo2 = 19-x
                 pos2+=velo2
         else:
-            if onGround(height,walls,dim,pos2):
+            if onGround(height,wall,dim,pos2):
                 if event.key == event.K_w or event.key == event.K_SPACE or event.key == event.K_UP:
                     velo2=25
                     pos2+=velo2
@@ -59,7 +59,7 @@ def moveTick(pos1,pos2,velo1,velo2,height,walls,event,dim,pWidth,pHeight):#velo 
             if event.key == event.K_S or event.key == event.K_UP:
                 velo2=-0
         else:
-            if onGround(height,walls,dim,pos2):
+            if onGround(height,wall,dim,pos2):
                 if event.key == event.K_w or event.key == event.K_SPACE or event.key == event.K_UP:
                     velo2=25
                     pos2+=velo2
@@ -70,6 +70,6 @@ def moveTick(pos1,pos2,velo1,velo2,height,walls,event,dim,pWidth,pHeight):#velo 
                 pos2+=velo2
     return pos1,pos2,velo1,velo2
 
-def onGround(height,walls,dim,pos):
+def onGround(height,wall,dim,pos):
     pass
     #to be implimented for based on needs
