19ce40a92ae9ea2c3b5d55e90230bb3d39991275
Connor Michalec <connormichalec@gmail.com>
2020-02-22 22:52:49 -0600

    a

--- GenerateMap.py
+++ GenerateMap.py
@@ -18,19 +18,16 @@ def getLevelMap(paths):
 
     #go through the slices and construct the map based off that, we don't actually need a top down map because these slices will make it
     for i in range(len(paths)): #each image represents an row.
-        map.append([])
+        map.append([]) #for each row
 
         for x in range(imageXW): #now insde of this one row, deal with each cell  individually
-            map[i].append([ ]) #create new cell in map
             
-            cell = []
+            cell = [] #holds the cell
             for y in range(imageYW): #y is the height, so it needs to cycle through each possible block in this height stack
                 if(Image.open(paths[i]).getpixel((x,y))[0:3] == key["WALL"]):
                     cell.append(["wall", imageYW-y])
-                    print(cell)
-            
-            map[i][x].append(cell) #i represents y, x represents x(aka the cell its on), and we need to add each block to this, so in the end we should have something like: [[[ ]]]
+
+            map[i].append(cell) #i represents y, x represents x(aka the cell its on), and we need to add each block to this, so in the end we should have something like: [[[ ]]]
                 #cell will now contain the various blocks at different heights for one cell
 
-    print(map)  
     return(map)
