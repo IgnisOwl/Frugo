fd0deeb1348f8d51886d516482f7175d3630b025
Ryan To <ryankto1@gmail.com>
2020-02-22 19:20:07 -0600

    Player Movements

--- moveTick.py
+++ moveTick.py
@@ -13,7 +13,7 @@ def moveTick(pos1,pos2,velo1,velo2,height,walls,event,dim):#vlo 1 is left right,
             if event.key == event.K_D or event.key == event.K_DOWN:
                 velo2=20
                 pos2+=velo2
-            if event.key == event.K_S or event.key == event.K_UP or event.key == event.K_SPACE:
+            if event.key == event.K_S or event.key == event.K_UP:
                 velo2=-20
                 pos2+=velo2
         else:
@@ -27,10 +27,25 @@ def moveTick(pos1,pos2,velo1,velo2,height,walls,event,dim):#vlo 1 is left right,
                     velo2=-40
                 pos2+=velo2
     elif event.type == event.KEYUP:
-        if event.key == event.K_a or event.key == event.K_LEFT:
+        if event.key == event.K_A or event.key == event.K_LEFT:
             velo1=0
-        if event.key == event.K_d or event.key == event.K_RIGHT:
+        if event.key == event.K_D or event.key == event.K_RIGHT:
             velo1=0
+        if not dim:
+            if event.key == event.K_D or event.key == event.K_DOWN:
+                velo2=0
+            if event.key == event.K_S or event.key == event.K_UP:
+                velo2=-0
+        else:
+            if onGround(height,walls,dim,pos2):
+                if event.key == event.K_w or event.key == event.K_SPACE or event.key == event.K_UP:
+                    velo2=25
+                    pos2+=velo2
+            else:
+                velo2-=1
+                if velo2<-40:
+                    velo2=-40
+                pos2+=velo2
     return pos1,pos2,velo1,velo2
 
 def onGround(height,walls,dim,pos):
