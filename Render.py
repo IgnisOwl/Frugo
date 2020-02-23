30b3daf3927eecd17a15629cc96f125e2dcef76e
julian <jjhennessy1231@gmail.com>
2020-02-22 23:41:16 -0600

    updating loop code

--- Render.py
+++ Render.py
@@ -10,18 +10,19 @@ class Render:
         self.screen.fill(self.colors["WHITE"])
         
         #get the current object we are handling in all the objects provided
-        for object in objects:
+        for sliceIndex in range(len(objects)):
             #if the object is a player, get the sprite based on the facing, and gun position
-            if(object[0][0:6] == "player"):
-                if(object[3] == "right"): #facing right
-                    objectImg = self.pygame.image.load(sprite_image_paths["player_1"])
-                elif(object[3] == "left"):
-                    objectImg = self.pygame.image.load(sprite_image_paths["player_2"])
+            for cellIndex in range(len(objects[sliceIndex])):
+                print(objects[sliceIndex][cellIndex])
+        
+                  #  objectImg = self.pygame.image.load(sprite_image_paths["player_1"])
+                #elif(object[3] == "left"):
+                #    objectImg = self.pygame.image.load(sprite_image_paths["player_2"])
                     
-            if(object[0][0:6] == "wall"):
+            #if(object[0][0:6] == "wall"):
                 objectImg = self.pygame.image.load(sprite_image_paths["solid_wall"])
                     
-            objectX = object[1]
+            objectX = 
             objectY = object[2]
             
             objectImg = self.pygame.transform.scale(objectImg, (self.sprite_size * self.size_multiplier, self.sprite_size * self.size_multiplier))
