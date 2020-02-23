60a4c3d7ae19d155cfc352db5b6f1b9558f37de3
julian <jjhennessy1231@gmail.com>
2020-02-23 02:03:09 -0600

    stuff

--- Render.py
+++ Render.py
@@ -6,30 +6,52 @@ class Render:
         self.size_multiplier = sizeMultiplier
         self.sprite_size = spriteSize
     
-    def render(self, objects, sprite_image_paths):
+    def render(self, objects, sprite_image_paths, dimension, playerX, playerY):
         self.screen.fill(self.colors["WHITE"])
         
         #get the current object we are handling in all the objects provided
         for sliceIndex in range(len(objects)):
             #if the object is a player, get the sprite based on the facing, and gun position
             for cellIndex in range(len(objects[sliceIndex])):
+                if(dimension == 0):
                 #print(objects[sliceIndex])
                   #  objectImg = self.pygame.image.load(sprite_image_paths["player_1"])
                 #elif(object[3] == "left"):
                 #    objectImg = self.pygame.image.load(sprite_image_paths["player_2"])
-                if(len(objects[sliceIndex][cellIndex])> 0):
+                    if(len(objects[sliceIndex][cellIndex])> 0): #if there are objects in the cell
                     #print(objects[sliceIndex][cellIndex])
-                    if(objects[sliceIndex][cellIndex][0][0] == "wall"): #0 is automatically the top, because the way the map generates, the top is added in last
-                    #if(objects[sliceIndex][cellIndex][self.getTop(objects[sliceIndex][cellIndex])][0] == "wall"):
-                        objectImg = self.pygame.image.load(sprite_image_paths["solid_wall"])
-                else:
-                    objectImg = self.pygame.image.load(sprite_image_paths["player_1"])
+                        objectType = objects[sliceIndex][cellIndex][0][0] #first 0 is to access the top object in the cell, second is to access the type
+                        if(objectType == "spawn"): #0 is automatically the top, because the way the map generates, the top is added in last
+                            objectImg = self.pygame.image.load(sprite_image_paths["spawn"])
+                        elif(objectType == "wall"):
+                            objectImg = self.pygame.image.load(sprite_image_paths["wall"])
+                        elif(objectType == "portal"):
+                            portalType = objects[sliceIndex][cellIndex][0][2]
+                            if(portalType == 0):
+                                objectImg = self.pygame.image.load(sprite_image_paths["portal_1"])
+                            elif(portalType == 1):
+                                objectImg = self.pygame.image.load(sprite_image_paths["portal_2"])
+                            elif(portalType == 2):
+                                objectImg = self.pygame.image.load(sprite_image_paths["portal_3"])
+                            elif(portalType == 3):
+                                objectImg = self.pygame.image.load(sprite_image_paths["portal_4"])
+                        elif(objectType == "goal"):
+                            objectImg = self.pygame.image.load(sprite_image_paths["goal"])
                     
-                objectX = round(cellIndex * self.sprite_size * self.size_multiplier)
-                objectY = round((len(objects) - sliceIndex) * self.sprite_size * self.size_multiplier)
+                        objectX = round(cellIndex * self.sprite_size * self.size_multiplier)
+                        objectY = round((len(objects) - sliceIndex) * self.sprite_size * self.size_multiplier)
             
-                objectImg = self.pygame.transform.scale(objectImg, (round(self.sprite_size * self.size_multiplier), round(self.sprite_size * self.size_multiplier)))
-                self.screen.blit(objectImg, (objectX, objectY))
+                        objectImg = self.pygame.transform.scale(objectImg, (round(self.sprite_size * self.size_multiplier), round(self.sprite_size * self.size_multiplier)))
+                        self.screen.blit(objectImg, (objectX, objectY))
+                    
+                    
+                    playerImg = self.pygame.image.load(sprite_image_paths["player"])
+                    self.screen.blit(playerImg, (playerX, playerY))
+                    
+                elif(dimension == 1):
+                    print("fuck you")
+                elif(dimensino == 2):
+                    print("fuck you x2")
         
         self.pygame.display.update()
 
